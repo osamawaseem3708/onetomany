fbb6cd7964e3b757f8844e7e81aace865b4a2a41
osamawaseem3708 <osama.ftc@gmail.com>
2022-12-29 17:19:36 +0500

    make uom change and reformat code...

--- functions.php
+++ functions.php
@@ -1,14 +1,35 @@
 <?php
 require('connection.php');
 
-function getUomDropDownOptions($con)
+function getProductDropDownOptions($con)
 {
-    $result=mysqli_query($con,"SELECT * FROM `tbluom`");
+    $result = mysqli_query($con, 'select * from tblproduct where stockinhand > 0');
     $select = ' <option value="">-- Choose --</option>';
     while ($row = mysqli_fetch_assoc($result)) {
-        $select .= '<option value="' . $row['longname'] . '">' .  $row['longname'] . '</option>';
+        $select .= '<option value="' . $row['prodid'] . '">' . $row['prodname'] . '</option>';
     }
     echo $select;
 }
+
+function getUomDropDownOptions($con)
+{
+    $result = mysqli_query($con, "SELECT * FROM `tbluom`");
+    $select = ' <option value="">-- Choose --</option>';
+    while ($row = mysqli_fetch_assoc($result)) {
+        $select .= '<option value="' . $row['longname'] . '">' . $row['longname'] . '</option>';
+    }
+    echo $select;
+}
+
+function getAllUoms($con)
+{
+    $uom_name_array = $uom_divide_by_array = [];
+    $result = mysqli_query($con, "SELECT * FROM `tbluom`");
+    while ($single_uom = mysqli_fetch_assoc($result)) {
+        $uom_name_array[$single_uom['longname']] = $single_uom['divideby'];
+    }
+    echo json_encode($uom_name_array);
+}
+
 ?>
 
